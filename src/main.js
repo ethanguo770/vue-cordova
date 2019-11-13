2aa51adba1119d9607bd7b82428428a490a728a7
zhixiankwok <zhixiankwok@gmail.com>
2019-11-13 17:22:58 +0800

    1. cordova + vue 项目初始化

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueCordova from 'vue-cordova'
 
+
+Vue.use(VueCordova)
 Vue.config.productionTip = false
+
+
+if (window.location.protocol === 'file:' || window.location.port === '3000') {
+  var cordovaScript = document.createElement('script')
+  cordovaScript.setAttribute('type', 'text/javascript')
+  cordovaScript.setAttribute('src', 'cordova.js')
+  document.body.appendChild(cordovaScript)
+}
 
 new Vue({
   router,
